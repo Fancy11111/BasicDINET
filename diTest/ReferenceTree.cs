9fff83c63431fc80f0c4f00b5d8c12d0e274d15f
Daniel Fenz <DFenz@rs-soft.com>
2020-07-23 16:49:27 +0200

    added dependencytree stub

---
+++ diTest/ReferenceTree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace diTest
+{
+	public class DependencyNode
+	{
+		public Type Value { get; set; }
+		public DependencyNode[] References { get; set; }
+		public DependencyNode[] Dependency { get; set; }
+	}
+
+	public class DependencyTree
+	{
+		
+		private IEnumerable<Type> registered = new Type[] { };
+
+		public void Insert(Type t)
+		{
+			if (registered.Contains<Type>(t)) return;
+			registered = registered.Append<Type>(t);
+
+		}
+	}
+}
